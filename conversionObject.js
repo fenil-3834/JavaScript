b91f01bd1c7c161d0e7993a7f9e4e1bf1db08b35
Fenil <137168917+fenil-3834@users.noreply.github.com>
2023-09-25 12:45:22 +0530

    operation

--- conversionObject.js
+++ conversionObject.js
@@ -3,8 +3,8 @@ const score = undefined;
 // console.log(typeof score);
 
 const valuesInNumber = Number(score);
-console.log(typeof valuesInNumber); // number
-console.log(valuesInNumber); // NaN (Not A Number)
+// console.log(typeof valuesInNumber); // number
+// console.log(valuesInNumber); // NaN (Not A Number)
 
 //Data Types Converted to Number with Output------------------>
 // "33" => 33
@@ -16,7 +16,7 @@ console.log(valuesInNumber); // NaN (Not A Number)
 const isLoggedIn = 0;
 
 const valuesInboolean = Boolean(isLoggedIn);
-console.log(valuesInboolean);
+// console.log(valuesInboolean);
 
 //Data Types Converted to Boolean with Output------------------>
 // 1 => true; 0 => false
@@ -26,4 +26,24 @@ console.log(valuesInboolean);
 const someNumber = 33;
 
 const valueInString = Array(someNumber);
-console.log(valueInString);
+// console.log(valueInString); //[ <33 empty items> ]
+
+//*********************OPERATIONS******************************** */
+
+let Values = 33;
+let negValues = -Values;
+
+// console.log(negValues); //-33
+
+// console.log(2 + 2); //4
+// console.log(2 - 2); //0
+// console.log(2 * 2); //4
+// console.log(2 ** 3); //8 (remender)
+// console.log(2 / 2); //1
+// console.log(2 % 3); //2 (modulas)
+
+let str1 = "Hello";
+let str2 = " fenil";
+
+let str3 = str1 + str2;
+// console.log(str3); //Hello fenil
