3294f9f1c20c9552c67be36419690c5e1565dd92
Fenil <137168917+fenil-3834@users.noreply.github.com>
2023-09-26 10:46:48 +0530

    Loops2

--- LoopsORItretion.js
+++ LoopsORItretion.js
@@ -9,11 +9,17 @@
 // }
 
 for (let i = 1; i <= 10; i++) {
-  console.log(`outer loop: ${i}`);
+  // console.log(`outer loop: ${i}`);
   for (let j = 1; j <= 10; j++) {
-    // console.log(`inner loop: ${j} and inner loop: ${i}`);
-    console.log(i + "x" + j + "=" + i * j);
+    // console.log(i + "x" + j + "=" + i * j);
   }
+}
+
+let myarray = ["Fenil", "Meet", "Arth"];
+// console.log(myarray.length);
+for (let i = 0; i < myarray.length; i++) {
+  const element = myarray[i];
+  // console.log(element);
 }
 
 // pattern
@@ -27,3 +33,43 @@ for (let i = 1; i <= 10; i++) {
 //   }
 // }
 // pattern(10);
+
+// keywords (break and continue)
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 5) {
+    // console.log("detedted 5");
+    break;
+  }
+  // console.log(`value of i is ${i}`);
+}
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 5) {
+    // console.log("detedted 5");
+    continue;
+  }
+  // console.log(`value of i is ${i}`);
+}
+
+// while loop
+let i = 0;
+while (i <= 10) {
+  // console.log(`value of i is ${i}`);
+  i = i + 2;
+}
+
+// while loop array
+let myarray1 = ["Fenil", "Meet", "Arth"];
+let arr = 0;
+while (arr < myarray1.length) {
+  // console.log(`value of arr is ${myarray1[arr]}`);
+  arr++;
+}
+
+// do while loop
+let score = 11;
+do {
+  // console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
